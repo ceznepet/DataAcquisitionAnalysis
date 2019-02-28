53d86c3cd00f8caec94d2a80246aedb2d5fe76c0
Petr Cezner <ceznepet@gmail.com>
2019-02-28 19:10:39 +0100

    Add project files.

---
+++ DataAcquiaitionAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataAcquiaitionAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
