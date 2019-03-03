5fe4f4de3356d8905fb913aaf7e505fc4ca0e5d2
Petr Cezner <ceznepet@gmail.com>
2019-03-03 16:08:38 +0100

    Correct type for conversion is int

--- KunbusRevolutionPiModule/TestOfKunbus.cs
+++ KunbusRevolutionPiModule/TestOfKunbus.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Threading;
 using KunbusRevolutionPiModule.Conversion;
 using KunbusRevolutionPiModule.KunbusPNS;
@@ -16,7 +17,7 @@ namespace KunbusRevolutionPiModule
         public TestOfKunbus(uint numberOfBytes, bool endian)
         {
             NumberOfBytes = numberOfBytes;
-            _config = new ProfinetIOConfig {Period = 4, BigEndian = endian};
+            _config = new ProfinetIOConfig {Period = 500, BigEndian = endian};
 
             KunbusRevolutionPiWrapper.piControlOpen();
             _samplerThread = new Thread(GatherData);
@@ -49,19 +50,55 @@ namespace KunbusRevolutionPiModule
 
                 if (_config.BigEndian ^ BitConverter.IsLittleEndian) Array.Reverse(outData);
 
-                var pokus = outData.OutputConversion(new float()) / 1000f;
+                var pokus = outData.OutputConversion(new int()) / 1000;
 
                 if (profinetIocStatus > 0)
                 {
                     foreach (var bit in outData) Console.Write("{0}, ", bit);
                     Console.WriteLine();
-                    Console.WriteLine(pokus);
+                    Console.WriteLine("From convertor: {0}",pokus);
+                    Console.WriteLine("From ByteArray: {0}",FromByteArray(outData, 0) / 1000);
                 }
                 else
                 {
                     Console.WriteLine("Hups...");
                 }
             }
+        }
+
+        public static float FromByteArray(byte[] arr, int ix = 0)
+        {
+            var uitos = new UInt32ToSingle
+            {
+                Byte0 = arr[ix],
+                Byte1 = arr[ix + 1],
+                Byte2 = arr[ix + 2],
+                Byte3 = arr[ix + 3],
+            };
+
+            return uitos.Single;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct UInt32ToSingle
+        {
+            [FieldOffset(0)]
+            public uint UInt32;
+
+            [FieldOffset(0)]
+            public float Single;
+
+            [FieldOffset(0)]
+            public byte Byte0;
+
+            [FieldOffset(1)]
+            public byte Byte1;
+
+            [FieldOffset(2)]
+            public byte Byte2;
+
+            [FieldOffset(3)]
+            public byte Byte3;
         }
     }
 }
