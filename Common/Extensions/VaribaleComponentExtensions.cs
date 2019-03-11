5ce437968bec1fa887e688b1ddde006856646c52
Petr Cezner <petr.cezner@factorio.cz>
2019-03-11 15:25:19 +0100

    Add extension

---
+++ Common/Extensions/VaribaleComponentExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Extensions
+{
+    public static class VaribaleComponentExtensions
+    {
+        public static DateTime ToDateTime(this List<VariableComponent> components)
+        {
+            if(components.Count != 7)
+            {
+                return new DateTime();
+            }
+            return new DateTime((int)components[0].Value,  //Year
+                                (int)components[1].Value,  //Month
+                                (int)components[2].Value,  //Day
+                                (int)components[3].Value,  //Hours
+                                (int)components[4].Value,  //Minutes
+                                (int)components[5].Value,  //Seconds
+                                (int)components[6].Value); //Milliseconds
+        }
+    }
+}
