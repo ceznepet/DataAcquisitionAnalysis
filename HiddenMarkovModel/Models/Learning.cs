a96fb5ec1ca59de1c1ef308213208acd5c54c62a
Petr Cezner <ceznepet@gmail.com>
2019-03-13 11:28:33 +0100

    Add project for Markov model

---
+++ HiddenMarkovModel/Models/Learning.cs
@@ -0,0 +1,17 @@
+﻿using Accord.Statistics.Distributions.Multivariate;
+using Accord.Statistics.Models.Markov.Learning;
+
+namespace HiddenMarkovModel.Models
+{
+    public class Learning
+    {
+        private HiddenMarkovClassifierLearning<MultivariateNormalDistribution, double[]> Teacher {get; set; }
+        private MultivariateNormalDistribution InitialDistribution { get; set; }
+        private string Folder { get; set; }
+
+        public Learning(string folder)
+        {
+            Folder = folder;
+        }
+    }
+}
