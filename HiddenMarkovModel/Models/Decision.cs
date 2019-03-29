0677a56163b3da9c1f16e9606ebf83bdfda0a87d
Petr Cezner <ceznepet@gmail.com>
2019-03-29 17:58:39 +0100

    Add classifier probability

--- HiddenMarkovModel/Models/Decision.cs
+++ HiddenMarkovModel/Models/Decision.cs
@@ -9,11 +9,13 @@ namespace HMModel.Models
     public class Decision
     {
         public double Probability { get; set; }
+        public double ClassifierProbability { get; set; }
         public int State { get; set; }
 
 
-        public Decision(double probability, int state)
+        public Decision(double classifier, double probability, int state)
         {
+            ClassifierProbability = classifier;
             Probability = probability;
             State = state;
         }
